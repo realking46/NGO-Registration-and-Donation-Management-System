14ca76d9847de5ec1447834aa305f695d64c1c14
Nishant Singh <161851576+realking46@users.noreply.github.com>
2026-01-21 04:58:34 +0530

    Add files via upload

---
+++ src/config/permissions.js
@@ -0,0 +1,16 @@
+export const PERMISSIONS = {
+  VIEW_USERS: "view_users",
+  FILTER_USERS: "filter_users",
+  EXPORT_USERS: "export_users",
+
+  VIEW_DONATIONS: "view_donations",
+  VIEW_DONATION_STATS: "view_donation_stats",
+  EXPORT_DONATIONS: "export_donations",
+
+  VERIFY_PAYMENTS: "verify_payments",
+
+  ASSIGN_ADMIN: "assign_admin",
+  UPDATE_PERMISSIONS: "update_permissions",
+
+  VIEW_AUDIT_LOGS: "view_audit_logs"
+};
